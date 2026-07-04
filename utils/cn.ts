226b85794b355ead8af411e5a0cebe61ff5e6d7a
Olamidepy <ooluwadamilare356@gmail.com>
2026-07-04 22:52:36 +0100

    docs: document cn utility styling merger function

--- utils/cn.ts
+++ utils/cn.ts
@@ -1,6 +1,13 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Merges class names dynamically.
+ * Combines clsx for conditional class resolution and tailwind-merge to clean up conflicting Tailwind classes.
+ * 
+ * @param inputs - Array of class values or conditional objects.
+ * @returns A consolidated, optimized className string.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
